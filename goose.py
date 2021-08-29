3b5d99ffd6fbf4b612c9e444bc5065180f8055f4
Chris Blackburn <krizboy@vt.edu>
2021-08-29 00:38:38 -0400

    can get epic weekly free games

--- goose.py
+++ goose.py
@@ -3,12 +3,14 @@
 import os
 import sys
 import asyncio
+import requests
 
 import discord
 from discord.ext import commands
 
 import logging
 
+logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
@@ -20,34 +22,57 @@ async def nest(ctx, minutes: int = 30):
     self.stop_poll()
 
     minutes = max(1, minutes)
-    self.poll_task = self.loop.create_task(self.poll(ctx.channel, minutes))
+    self.poll_task = self.loop.create_task(self.poll(ctx.channel.id, minutes))
     await ctx.send("I will be nesting here now, see you in {} min".format(
         minutes))
 
 @commands.command()
 async def honk(ctx):
     self = ctx.bot
-    log.info("Checking for golden eggs")
+    self.log.info("Checking for golden eggs")
     await ctx.send(await self.do_update())
 
 class Goose(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        intents = discord.Intents.none() # TODO: restrict after testing
+    def __init__(self, logger, *args, **kwargs):
+        intents = discord.Intents.default()
         intents.messages = True
 
-        super().__init__(command_prefix=DEFAULT_PREFIX,
-                guild_subcriptions=False, intents=intents, *args, **kwargs)
+        super().__init__(command_prefix=DEFAULT_PREFIX, intents=intents, *args, **kwargs)
 
         self.add_command(nest)
         self.add_command(honk)
 
-    async def do_update(self):
-        return "Sorry, no new golden eggs"
+        self.log = logger
 
-    async def poll(self, channel: discord.TextChannel, minutes=30):
+    async def do_update(self):
+        r = requests.get("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions")
+        if r.status_code != 200:
+            self.log.info("Received {} from epic games".format(r.status_code))
+            return "Sorry, no new golden eggs"
+
+        body = r.json()
+
+        def weekly(game):
+            if game["promotions"] and \
+                    len(game["promotions"]["promotionalOffers"]):
+                return True
+
+            return False
+
+        free_games = filter(weekly,
+                body["data"]["Catalog"]["searchStore"]["elements"])
+        titles = map(lambda game : game["title"], free_games)
+        return "Awwww yeah, sweet golden eggs: {}".format(list(titles))
+
+    async def poll(self, channel_id: int, minutes: int = 30):
         try:
             while not self.is_closed():
                 await asyncio.sleep(max(60, minutes * 60))
+                self.log.debug("Getting channel from id: {}".format(channel_id))
+                channel = self.get_channel(channel_id)
+                if not channel:
+                    self.poll_task = None
+                    break
                 await channel.send(await self.do_update())
         except asyncio.CancelledError:
             pass
@@ -63,5 +88,5 @@ if __name__=="__main__":
         log.error("Please set the TOKEN environment variable")
         sys.exit(1)
 
-    goose = Goose()
+    goose = Goose(log)
     goose.run(token)
